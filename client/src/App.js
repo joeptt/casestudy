058cc78014218019a9e3a00a93e3f5fec53e5cf8
Joe Petereit <joe.petereit@web.de>
2022-11-22 13:37:11 +0100

    button functionality updated

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 import Ride from "./components/ride";
 
 export default function App() {
+    const opacity = 100;
+    const clickedOpacity = 50;
     const [allRides, setAllRides] = useState();
     const [ridesDisplayed, setRidesDisplayed] = useState();
     const [fromButtonClicked, setFromButtonClicked] = useState(false);
     const [toButtonClicked, setToButtonClicked] = useState(false);
+    const [fromButtonOpacity, setFromButtonOpacity] = useState(opacity);
+    const [toButtonOpacity, setToButtonOpacity] = useState(opacity);
 
     useEffect(() => {
+        // get all connections from server
         fetch("/rides")
             .then((res) => res.json())
             .then((data) => {
@@ -16,10 +21,19 @@ export default function App() {
             });
     }, []);
 
+    // onClick to display only connections with departure from Frankfurt
     const fromFrankfurt = () => {
+        // unfocus the "Nach Frankfurt"-button
+        setToButtonClicked(false);
+        setToButtonOpacity(opacity);
         if (fromButtonClicked) {
+            // if the button is currently focused display all connections again and raise opacity
+            setFromButtonOpacity(opacity);
             setRidesDisplayed(allRides);
         } else {
+            // if the button is currently not focused, set opacity low and iterate through all connections to find the
+            // ones going to Frankfurt
+            setFromButtonOpacity(clickedOpacity);
             const arr = [];
             for (let i = 0; i < allRides.length; i++) {
                 if (allRides[i].from === "Frankfurt(Main)Hbf") {
@@ -31,10 +45,15 @@ export default function App() {
         setFromButtonClicked(!fromButtonClicked);
     };
 
+    // onClick to display only connections with arrival to Frankfurt
     const toFrankfurt = () => {
+        setFromButtonClicked(false);
+        setFromButtonOpacity(opacity);
         if (toButtonClicked) {
+            setToButtonOpacity(opacity);
             setRidesDisplayed(allRides);
         } else {
+            setToButtonOpacity(clickedOpacity);
             const arr = [];
             for (let i = 0; i < allRides.length; i++) {
                 if (allRides[i].to === "Frankfurt(Main)Hbf") {
@@ -55,8 +74,18 @@ export default function App() {
             )}
             {ridesDisplayed && (
                 <div>
-                    <button onClick={fromFrankfurt}>Von Frankfurt</button>
-                    <button onClick={toFrankfurt}>Nach Frankfurt</button>
+                    <button
+                        style={{ opacity: `${fromButtonOpacity}%` }}
+                        onClick={fromFrankfurt}
+                    >
+                        Von Frankfurt
+                    </button>
+                    <button
+                        style={{ opacity: `${toButtonOpacity}%` }}
+                        onClick={toFrankfurt}
+                    >
+                        Nach Frankfurt
+                    </button>
                 </div>
             )}
             {ridesDisplayed ? (
